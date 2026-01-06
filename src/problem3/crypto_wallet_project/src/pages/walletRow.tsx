60ae308f60f8c4bf9401ae25ff988d235a3e1b80
ngyang94 <ngyangngyang@gmail.com>
2026-01-06 15:48:32 +0800

    create missing component "WalletRow" and import it to "WalletPage" and create the missing "classes.row" object variable

---
+++ src/problem3/crypto_wallet_project/src/pages/walletRow.tsx
@@ -0,0 +1,18 @@
+
+
+export default function WalletRow({amount,usdValue,formattedAmount,className}:{amount:number,usdValue:number,formattedAmount:string,className?:string[]}){
+
+    return <>
+        <div {...className}>
+            <div>
+                Amount: {amount}
+            </div>
+            <div>
+                USD: {usdValue}
+            </div>
+            <div>
+                Formatted Amount: {formattedAmount}
+            </div>
+        </div>
+    </>;
+}
